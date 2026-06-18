a5dfff71b79155cfcc4d6a37f4addc3a46d66d28
Silas Pignotti <pignottisilas@gmail.com>
2026-06-18 12:10:48 +0200

    chore: fix lint issues and finalize setup

--- src/berlin_lst_downscaling/__init__.py
+++ src/berlin_lst_downscaling/__init__.py
@@ -1,3 +1,4 @@
-"""Reproducible deep-learning pipeline that downscales Landsat LST from ~100 m to 10 m for Berlin."""
+"""Reproducible deep-learning pipeline that downscales Landsat LST from
+~100 m to 10 m for Berlin."""
 
 __version__ = "0.1.0"
